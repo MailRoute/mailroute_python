6581353d1db5d8fe9b0117964391805f494bd86f
Alexander Moiseenko <imdagger@yandex.ru>
2013-03-27 02:02:13 +0300

    added tests for all items fetching

--- tests/test_queryset.py
+++ tests/test_queryset.py
@@ -45,15 +45,90 @@ class TestQueries(unittest.TestCase):
             reseller.delete()
 
     def test_filter(self):
-        False.should.be.ok
+        N = 15
+        prefix = uuid.uuid4().hex
+
+        for i in xrange(N):
+            new_name = '{1} Reseller A N{0}'.format(i, prefix)
+            new_one = mailroute.Reseller.create(name=new_name)
+            new_name = '{1} Reseller B N{0}'.format(i, prefix)
+            new_one = mailroute.Reseller.create(name=new_name)
+
+        resellers = mailroute.Reseller.filter(name__startswith='{0} Reseller'.format(prefix))
+        len(resellers).should.be.equal(2 * N)
+        resellers = mailroute.Reseller.filter(name__startswith='{0} Reseller A'.format(prefix))
+        len(resellers).should.be.equal(N)
+        resellers = mailroute.Reseller.filter(name__startswith='{0} Reseller B'.format(prefix))
+        len(resellers).should.be.equal(N)
+        resellers = mailroute.Reseller.filter(name__startswith='{0} Reseller B'.format(prefix)).limit(3)
+        len(resellers).should.be.equal(3)
+        resellers = mailroute.Reseller.filter(name__startswith='{0} Reseller B'.format(prefix)).limit(3).offset(2)
+        len(resellers).should.be.equal(3)
+
+        resellers = mailroute.Reseller.filter(name__startswith='{0} Nothing'.format(prefix))
+        resellers.should.be.empty
+
+        resellers = mailroute.Reseller.filter(name='{0} Reseller A N0'.format(prefix))
+        len(resellers).should.be.equal(1)
+        resellers[0].name.should.be.equal('{0} Reseller A N0'.format(prefix))
+
+        mailroute.Reseller.filter.when.called_with(branding_info__color='red').should. \
+                                    throw('InvalidFilter')
+
+        for reseller in mailroute.Reseller.filter(name__startswith='{0}'.format(prefix)):
+            reseller.delete()
 
     @httpretty.httprettified
     def test_all_empty(self):
-        False.should.be.ok
+        httpretty.HTTPretty.register_uri(httpretty.HTTPretty.GET, 'https://ci.mailroute.net/api/v1/reseller/',
+                                         status=200, body=json.dumps({
+                                             'meta': {
+                                                 'limit': 20,
+                                                 'next': None,
+                                                 'offset': 0,
+                                                 'previous': None,
+                                                 'total_count': 0,
+                                                 }, 
+                                             'objects': []
+                                             }),
+                                         content_type='application/json')
+        query = mailroute.Reseller.all()
+        query.should.be.empty
+        query.fetch().should.be.empty
 
-    @httpretty.httprettified
     def test_all(self):
-        False.should.be.ok
+        obj = {
+            "absolute_url": "/reseller/615/", 
+            "allow_branding": False, 
+            "allow_customer_branding": False, 
+            "branding_info": "/api/v1/brandinginfo/622/", 
+            "contacts": [], 
+            "created_at": "Fri, 22 Mar 2013 18:39:01 -0700", 
+            "customers": [], 
+            "id": 615, 
+            "name": "095eeef1d4204022a2a4e028de9ef2eb Reseller N0", 
+            "resource_uri": "/api/v1/reseller/615/", 
+            "updated_at": "Fri, 22 Mar 2013 18:39:01 -0700"
+        }
+
+        query = mailroute.Reseller.Entity.is_actual() # force to load schema
+        httpretty.HTTPretty.enable()
+        PAGE = 20
+        httpretty.HTTPretty.register_uri(httpretty.HTTPretty.GET, 'https://ci.mailroute.net/api/v1/reseller/',
+                                         status=200, body=json.dumps({
+                                            'meta': {
+                                                 'limit': PAGE,
+                                                 'next': '/api/v1/reseller/?limit=20&offset=20',
+                                                 'offset': 0,
+                                                 'previous': None,
+                                                 'total_count': PAGE * 3,
+                                            }, 
+                                            'objects': [obj] * PAGE
+                                        }),
+                                        content_type='application/json')
+        query = mailroute.Reseller.all()
+        len(query).should.be.equal(PAGE * 3)
+        httpretty.HTTPretty.disable()
 
     def test_get(self):
         resellers = mailroute.Reseller.filter(name__startswith='Testing').limit(20)
